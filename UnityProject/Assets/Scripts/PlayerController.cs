605cb7756b5bd3862303ec74b6287788048e16db
Hans <hnsgunther@gmail.com>
2021-09-18 01:44:50 -0600

    Updated rotation. Added skybox

--- UnityProject/Assets/Scripts/PlayerController.cs
+++ UnityProject/Assets/Scripts/PlayerController.cs
@@ -7,7 +7,7 @@ using UnityEngine.InputSystem.Interactions;
 public class PlayerController : MonoBehaviour
 {
     public float rotateSpeed;
-    
+    public float maxRotation;    
     private Vector2 m_rotationInput;
 
     void Start()
@@ -21,14 +21,39 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
-        Rotate(m_rotationInput);
+        
     }
 
     private void Rotate(Vector2 rotationInput){
-        transform.Rotate(new Vector3(rotationInput.x, 0.0f, rotationInput.y));
+        if (rotationInput.magnitude < 0.1){
+            return;
+        }
+
+        var rotation = transform.eulerAngles;
+        if (Mathf.Abs(rotationInput.x) > 0.1){
+            rotation.x += rotationInput.x * rotateSpeed * Time.deltaTime;
+            if (rotation.x > 180){
+                rotation.x = Mathf.Max(rotation.x, 360 -maxRotation);
+            }
+            else{
+                rotation.x = Mathf.Min(rotation.x, maxRotation);
+            }
+        }
+        rotation.y = 0.0f;
+        if (Mathf.Abs(rotationInput.y) > 0.1){
+            rotation.z += rotationInput.y * rotateSpeed * Time.deltaTime;
+            if (rotation.z > 180){
+                rotation.z = Mathf.Max(rotation.z, 360 -maxRotation);
+            }
+            else{
+                rotation.z = Mathf.Min(rotation.z, maxRotation);
+            }
+        }
+        transform.eulerAngles = rotation;
     }
 
     void FixedUpdate()
     {
+        Rotate(m_rotationInput);
     }
 }
